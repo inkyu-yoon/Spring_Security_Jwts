2730bdbe78b146dde1a4834d855d5e727a2805aa
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-12-06 13:17:02 +0900

    ADD : HTTP 시큐리티 설정

---
+++ src/main/java/practice/security/filter/HttpSecurityConfig.java
@@ -0,0 +1,28 @@
+package practice.security.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class HttpSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("api/v1/login").permitAll()
+                .antMatchers(HttpMethod.POST, "api/v1/**").authenticated()
+                .and()
+                .sessionManagement()
+                .and()
+                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
